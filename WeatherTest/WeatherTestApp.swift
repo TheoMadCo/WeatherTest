a22a3782292ae6a2e4b2611ed9d1efbfa2affa65
Matteo Noro <86775603+TheoMadCo@users.noreply.github.com>
2024-04-20 14:29:39 +0200

    Initial Commit

---
+++ WeatherTest/WeatherTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTestApp.swift
+//  WeatherTest
+//
+//  Created by Matteo Noro on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
